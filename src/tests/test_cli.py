1ec27c82a7256747aa11dbdfd8a276d88736fdfa
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2024-11-11 22:23:44 +0100

    add test for check_sufficient_data_available

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -57,9 +57,9 @@ def test_can_find_meter_values_for_customer(fixtures_path: Path) -> None:
     invoice = Invoice("12345", 2021, 7, "customers.csv", fixtures_file)
     values = invoice.get_meter_values()
     assert len(values) == 4
-    assert values[0]["date"] == "2021-01-03"
+    assert values[0]["date"] == "2021-03-02"
     assert values[1]["customer"] == "12345"
-    assert values[2]["value"] == "200"
+    assert values[2]["value"] == "100"
 
 
 def test_raises_error_if_date_not_found(fixtures_path: Path) -> None:
@@ -68,3 +68,40 @@ def test_raises_error_if_date_not_found(fixtures_path: Path) -> None:
     with pytest.raises(ValueError) as ex:
         invoice.check_date_exists()
     assert str(ex.value) == "Date with month 7 and year 2025 not found."
+
+
+def test_can_sort_dates(fixtures_path: Path) -> None:
+    fixtures_file = fixtures_path / "values.csv"
+    invoice = Invoice("12345", 2021, 7, "customers.csv", fixtures_file)
+    dates = invoice.get_sorted_dates()
+    assert dates == [
+        "2021-02-08",
+        "2021-03-02",
+        "2021-03-30",
+        "2021-04-27",
+    ]
+
+
+def test_can_check_sufficient_data_available_in_the_past(fixtures_path: Path) -> None:
+    fixtures_file = fixtures_path / "values.csv"
+    invoice = Invoice("12345", 2021, 1, "customers.csv", fixtures_file)
+    dates = invoice.get_sorted_dates()
+    with pytest.raises(ValueError):
+        invoice.check_sufficient_data_available_in_the_past(dates)
+
+
+def test_can_check_sufficient_data_available_in_the_future(fixtures_path: Path) -> None:
+    fixtures_file = fixtures_path / "values.csv"
+    invoice = Invoice("12345", 2021, 7, "customers.csv", fixtures_file)
+    dates = invoice.get_sorted_dates()
+    with pytest.raises(ValueError):
+        invoice.check_sufficient_data_available_in_the_future(dates)
+
+
+def test_can_check_sufficient_data_available(fixtures_path: Path) -> None:
+    fixtures_file = fixtures_path / "values.csv"
+    invoice = Invoice("12345", 2021, 7, "customers.csv", fixtures_file)
+    dates = invoice.get_sorted_dates()
+    with pytest.raises(ValueError) as ex:
+        invoice.check_sufficient_data_available(dates)
+    assert str(ex.value) == "Not sufficient data available"
